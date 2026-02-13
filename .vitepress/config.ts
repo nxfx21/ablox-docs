d97b73f0c1e3ecd8bd2d3aee9ea1cc115b77270a
Origaming <contact@origaming.ch>
2026-02-13 15:31:53 +0000

    Update GitHub edit link pattern in config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
       provider: 'local'
     },
     editLink: {
-      pattern: 'https://github.com/AppleBlox/appleblox/edit/main/docs/:path',
+      pattern: 'https://github.com/AppleBlox/docs/edit/main/:path',
       text: 'Edit this page on GitHub'
     },
     lastUpdated: {
